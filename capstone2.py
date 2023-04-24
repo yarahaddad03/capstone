4c8fb569cec3b2e555e95de508b8aa9a86a15ed4
yarahaddad03 <114431403+yarahaddad03@users.noreply.github.com>
2023-04-24 22:45:32 +0300

    Update capstone2.py

--- capstone2.py
+++ capstone2.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.naive_bayes import GaussianNB
 
 st.write("""
 # Students Performance Prediction App
@@ -12,7 +12,7 @@ Data obtained from the School of Business Admissions Office)
 
 st.sidebar.header('User Input Features')
 
-students_data = pd.read_csv('preprocessed-data.csv')
+students_data = pd.read_csv('C:/Users/lenovo/Desktop/Streamlit/predict/preprocesseddata.csv')
 
 def user_input_features():
     Age = st.sidebar.slider('Age at admission', 16, 50, 18)
@@ -20,8 +20,9 @@ def user_input_features():
     Country = st.sidebar.selectbox('Country', ('0', '1'))
     Scholarship = st.sidebar.selectbox('Scholarship', ('0', '1'))
     sat = st.sidebar.slider('SAT', 0, 1530, 1100)
-    Governorates = st.sidebar.selectbox('Governorates', -1, 7, 1)
-    School = st.sidebar.selectbox('School', -1, 6, 2)
+    Governorates = st.sidebar.slider('Governorates', 0, 7, 1)
+    School = st.sidebar.slider('School', 0, 100, 50)
+    Hours = st.sidebar.slider('hours_diff', 0, 100, 50)
     
     data = {'Age at admission': Age,
             'Gender': Gender,
@@ -29,7 +30,8 @@ def user_input_features():
             'Scholarship' : Scholarship,
             'SAT' : sat,
             'Governorates' : Governorates,
-            'School' : School}
+            'School' : School,
+            'hours_diff' : Hours}
     features = pd.DataFrame(data, index=[0])
     return features
 
@@ -38,7 +40,9 @@ df= user_input_features()
 st.subheader('User Input parameters')
 st.write(df)
 
-# Model
+#Model
+nb = GaussianNB()
+
 X = students_data.drop('Performance', axis=1)
 y = students_data['Performance']
 
@@ -48,15 +52,15 @@ from sklearn.metrics import accuracy_score, classification_report
 # Split the data into training and test sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, train_size=0.8, random_state=42)
 
-# Create and train the Random Forest model
-rf = RandomForestClassifier()
-rf.fit(X_train, y_train)
+# Create and train the Naive Bayes model
+model = GaussianNB()
+model.fit(X_train, y_train)
 
 # Predict the test data using the trained model
-y_pred = rf.predict(X_test)
+y_pred = model.predict(X_test)
 
-prediction = rf.predict(df)
-prediction_proba= rf.predict_proba(df)
+prediction = model.predict(df)
+prediction_proba= model.predict_proba(df)
 
 
 st.subheader('Prediction')
